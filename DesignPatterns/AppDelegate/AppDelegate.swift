5a7bd20df071112729f2bd5033fb9fc7fb4b750e
Ajay Babu <ajayiosdeveloper@gmail.com>
2018-11-22 11:30:08 +0530

    Added changes in AppDelegate

--- DesignPatterns/AppDelegate/AppDelegate.swift
+++ DesignPatterns/AppDelegate/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Praise the LORD!")
+        print("Praise the LORD Jesus Christ forever and forevermore.Amen His grace endures forever  and forevermore.Amen")
         return true
     }
 
